fd540c935abd10d7338097d21303c170116ec30a
RastkoSavic <rastkosavic23@gmail.com>
2018-04-09 21:14:39 +0200

    =Formatted timestamps using moment.js

---
+++ playground/time.js
@@ -0,0 +1,19 @@
+var moment = require('moment');
+
+// moment.locale('sr');
+
+// var date = moment();
+
+// date.add(100, 'years');
+
+// date.subtract(9, 'months');
+
+// console.log(date.format('MMM Do, YYYY'));
+
+var createdAt = 1234;
+
+var someTimestamp = moment().valueOf();
+
+var date = moment();
+
+console.log(someTimestamp);
