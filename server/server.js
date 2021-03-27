881ef0670624e2a2a7646a601c218b9f0ea3ade9
RastkoSavic <rastkosavic23@gmail.com>
2021-03-27 12:46:04 +0100

    testing origin 2

--- server/server.js
+++ server/server.js
@@ -84,5 +84,5 @@ io.on('connection', (socket) => {
 
 // Server Start
 server.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`);
+    console.log(`Server is up on port ${port}`);
 });
